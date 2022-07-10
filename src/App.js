ca2fa61b68f28ce5f0fe55a66547bd6e48e9d4a1
DiegoFabbrii <diegofabbri@outlook.com>
2022-07-10 01:46:52 -0300

    routes changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Header from "./components/Header/Header";
 import MenuMobProvider from "./context/MenuMobContext";
 import ThemeProvider from "./context/ThemeContext";
 import Home from "./pages/Home";
-import SingIn from "./pages/SingIn";
-import SingUp from "./pages/SingUp";
-import Account from "./pages/Account";
 import CoinPage from "./pages/CoinPage";
 import SearchAndPagProvider from "./context/SearchAndPaginationContext";
 
@@ -17,9 +14,6 @@ function App() {
                     <Header />
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/sigin" element={<SingIn />} />
-                        <Route path="/" element={<SingUp />} />
-                        <Route path="/" element={<Account />} />
                         <Route path="/coin/:id" element={<CoinPage />} />
                     </Routes>
                 </SearchAndPagProvider>
